059d7de94361cbc6d223755f1f006a5439b304bb
Bakhtiar Ahmed <114367140+BakhtiarAhmed41@users.noreply.github.com>
2022-10-25 23:33:26 +0500

    Create Lists_3.py

---
+++ Lists_3.py
@@ -0,0 +1,20 @@
+#  Write the corresponding Python assignment statements: 
+# (a)Assign 6 to variable a and 7 to variable b.
+# (b)Assign to variable c the average of variables a and b.
+# (c)Assign to variable inventory the list containing strings 'paper', 'staples', and 'pencils'. 
+# (d)Assign to variables first, middle and last the strings 'John', 'Fitzgerald', and 'Kennedy'.
+# (e)Assign to variable fullname the concatenation of string variables first, middle, and last. Make sure you incorporate blank spaces appropriately.
+
+a = 6
+b = 7
+
+c = (a + b) / 2
+
+inventory = ['paper', 'staples', 'pencils']
+print(inventory)
+
+first = "John "
+middle = "Fitzgerald "
+last = "Kennedy"
+fullname = first + middle + last
+print(fullname)
